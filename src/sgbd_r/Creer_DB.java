1f3cd306b5dd83a0100e10fef3f407004a0315f2
jessekamba <kambajesse@gmail.com>
2019-05-02 23:17:40 +0200

    creation de la base de donnees en verifiant si la requete correspond au sql standard

--- src/sgbd_r/Creer_DB.java
+++ src/sgbd_r/Creer_DB.java
@@ -23,12 +23,14 @@ public class Creer_DB {
         System.out.println("veuillez taper la requete pour creer votre base de donneee");
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
-        try {
-            ligne= reader.readLine();
-        } catch (IOException ex) {
-            Logger.getLogger(SGBD_R.class.getName()).log(Level.SEVERE, null, ex);
-        }
         
+         try {
+             ligne= reader.readLine();
+         } catch (IOException ex) {
+             Logger.getLogger(Creer_DB.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+         
            
     final String separateur=" ";
 //    String chaine_a_decouper=ligne;
@@ -45,8 +47,8 @@ public class Creer_DB {
     String recup_database=requete[1];
     String dbname=requete[2];
     
-//    if ("create".equals(recup_create) && "database".equals(recup_database))
-//    {
+    if ("create".equals(recup_create) && "database".equals(recup_database))
+    {
     String path="C:\\Users\\JESSE KAMBA\\Documents\\projet\\java\\"+dbname;
     File file = new File(path);
 //    boolean test= file.exists();
@@ -64,10 +66,10 @@ public class Creer_DB {
             System.err.println("une erreur interne s'est produite");
             }
         }
-//     }else {
-//        System.err.println("veuillez verifier votre requete");
-//        System.err.println("la requete se rapprochant de la votre est 'CREATE DATABASE DATABASE_NAME'");
-//    }
+     }else {
+        System.err.println("veuillez verifier votre requete");
+        System.err.println("la requete se rapprochant de la votre est 'CREATE DATABASE DATABASE_NAME'");
+    }
         
 //        System.out.println("la requete est "+ ligne);
     }
